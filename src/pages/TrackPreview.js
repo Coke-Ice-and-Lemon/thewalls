aa1a8f43ec36ce21eaa4677013524ae056fb6e0d
Poojan-20 <poojanp2008@gmail.com>
2023-09-04 23:48:08 +0530

    Co-authored-by: Harsh Mathur <harshmathurx@users.noreply.github.com>

--- src/pages/TrackPreview.js
+++ src/pages/TrackPreview.js
@@ -1,10 +1,30 @@
-import React, { useState } from 'react';
+import React, { useState,useEffect } from 'react';
 import Image from "next/image"
 const TrackPreview = ({ track }) => {
+
+    useEffect(() => {
+        // define a custom handler function
+        // for the contextmenu event
+        const handleContextMenu = (e) => {
+            // prevent the right-click menu from appearing
+            e.preventDefault()
+        }
+
+        // attach the event listener to 
+        // the document object
+        document.addEventListener("contextmenu", handleContextMenu)
+
+        // clean up the event listener when 
+        // the component unmounts
+        return () => {
+            document.removeEventListener("contextmenu", handleContextMenu)
+        }
+    }, [])
+
     const [isPlaying, setIsPlaying] = useState(false);
     const audioRef = React.createRef();
 
-    const handleMouseEnter = () => {
+    const Playpreview = () => {
         if (track.preview_url) {
             audioRef.current.src = track?.preview_url;
             audioRef.current.play();
@@ -12,7 +32,7 @@ const TrackPreview = ({ track }) => {
         }
     };
 
-    const handleMouseLeave = () => {
+    const Stoppreview = () => {
         if (isPlaying) {
             audioRef.current.pause();
             setIsPlaying(false);
@@ -20,7 +40,8 @@ const TrackPreview = ({ track }) => {
     };
 
     return (
-        <div onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
+        <div onMouseEnter={Playpreview} onMouseLeave={Stoppreview} onTouchStart={Playpreview}
+            onTouchEnd={Stoppreview}>
             <Image priority={true} className="w-full" src={track?.album?.images[0].url} height={640} width={640} alt="Sunset in the mountains" />
             <audio ref={audioRef}></audio>
         </div>
