80945bc27b8eae205c1f6302a1f3381abf35c22b
Poojan-20 <poojanp2008@gmail.com>
2023-09-05 23:07:37 +0530

    UI change

--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -9,9 +9,10 @@ const Tracks = ({ data }) => {
     const { data: session } = useSession()
     const router = useRouter()
     const time_range = router.query.time_range
+    const [timeRange, setTimeRange] = useState(time_range);
 
-    async function getTopTracks() {
-        const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${time_range}`, {
+    async function getTopTracks(time) {
+        const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${time}`, {
             headers: {
                 Authorization: `Bearer ${session.accessToken}`
             }
@@ -23,22 +24,46 @@ const Tracks = ({ data }) => {
     useEffect(() => {
         const f = async () => {
             if (session) {
-                setTracks(await getTopTracks())
+                setTracks(await getTopTracks(timeRange))
             }
         }
         f();
-    }, [session])
+    }, [session, timeRange])
+
+    useEffect(() => {
+        if(router.isReady){
+            setTimeRange(time_range)
+        }
+    },[time_range])
 
     return (
-        <div className='px-20 p-10 bg-[#000]'>
+        <div className='px-5 p-10 flex flex-col items-center justify-center'>
             {/* {JSON.stringify(tracks)} */}
-            <div className='flex flex-row flex-wrap h-full w-full justify-start'>
+            <h3 className='text-xl font-bold'>WHAT'S PLAYING</h3>
+            <ul className="flex flex-wrap text-xs sm:font-medium text-center text-gray-500 dark:text-gray-400 mb-10 justify-center mt-5">
+                <li className="mr-2">
+                    <div className={`inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white cursor-pointer ${timeRange == "short_term" && "text-white bg-blue-600"}`} aria-current="page" onClick={() => {
+                        router.push('/tracks?time_range=short_term')
+                    }}>Last Month</div>
+                </li>
+                <li className="mr-2">
+                    <div className={`inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white cursor-pointer ${timeRange == "medium_term" && "text-white bg-blue-600"}`} onClick={() => {
+                        router.push('/tracks?time_range=medium_term')
+                    }}>Last 6 Months</div>
+                </li>
+                <li className="mr-2">
+                    <div className={`inline-block px-2 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-white cursor-pointer ${timeRange == "long_term" && "text-white bg-blue-600"}`} onClick={() => {
+                        router.push('/tracks?time_range=long_term')
+                    }}>All Time</div>
+                </li>
+            </ul>
+            <div className='flex flex-row flex-wrap h-full w-full justify-center'>
                 {tracks && tracks.map((track) => (
                     (track.album.images && (
-                        <div className="max-w-sm rounded overflow-hidden mb-5 mx-4 hover:scale-105 
-                        transition duration-150 bg-zinc-900 ease-out hover:ease-in" key={track?.id}>
+                        <div className="w-[25%] sm:w-[20%] lg:w-[15%] xl:w-[12%] 2xl-[10%]  rounded overflow-hidden mb-3 mx-2 hover:scale-105 
+                        transition duration-150 ease-out hover:ease-in" key={track?.id}>
                             <TrackPreview track={track} />
-                            <div class="font-bold text-xl mb-2 mt-2 text-center">{track?.name}</div>
+                            {/* <div className="font-bold text-[0.5rem] mb-2 mt-2 px-2 text-center">{track?.name}</div> */}
                         </div>
                     ))
                 ))}
