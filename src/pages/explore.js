0a6f229a9a0e20d3739579d99515693af2971109
Poojan-20 <poojanp2008@gmail.com>
2023-09-05 23:09:03 +0530

    Ui change

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -9,15 +9,15 @@ const Explore = () => {
             <div className="flex flex-row w-5/6 lg:w-3/6 justify-between mt-10">
                 <Link href={{pathname:'tracks',query:{time_range:'short_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
                 px-1 py-2 lg:px-4 rounded">
-                    Short Term
-                </button></Link>
-                <Link href={{pathname:'tracks',query:{time_range:'long_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
-                px-1 py-2 lg:px-6 rounded">
-                    Long Term
+                    Last Month
                 </button></Link>
                 <Link href={{pathname:'tracks',query:{time_range:'medium_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
                 px-1 py-2 lg:px-8 rounded">
-                    Middle Term
+                    Last 6 Months
+                </button></Link>
+                <Link href={{pathname:'tracks',query:{time_range:'long_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
+                px-1 py-2 lg:px-6 rounded">
+                    All Time
                 </button></Link>
             </div>
         </div>
