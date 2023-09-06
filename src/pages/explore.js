649e6372f83dfed42637c120b2930131e15bab26
Poojan-20 <poojanp2008@gmail.com>
2023-09-07 01:05:54 +0530

    UI (made responsive design)

--- src/pages/explore.js
+++ src/pages/explore.js
@@ -5,17 +5,17 @@ const Explore = () => {
     
     return (
         <div className="w-full flex flex-col justify-center items-center mt-20">
-            <h2 className="text-2xl font-bold">Select the time period</h2>
-            <div className="flex flex-row w-5/6 lg:w-3/6 justify-between mt-10">
-                <Link href={{pathname:'tracks',query:{time_range:'short_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
+            <h2 className=" md:text-xl lg:text-2xl font-bold">Select the time period</h2>
+            <div className="flex flex-row w-5/6 lg:w-3/6 justify-center mt-10">
+                <Link href={{pathname:'tracks',query:{time_range:'short_term'}}}><button className="mr-3 bg-green-500 hover:bg-green-700 text-white font-bold text-[0.5rem] sm:text-sm md:text-md lg:text-lg 
                 px-1 py-2 lg:px-4 rounded">
                     Last Month
                 </button></Link>
-                <Link href={{pathname:'tracks',query:{time_range:'medium_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
+                <Link href={{pathname:'tracks',query:{time_range:'medium_term'}}}><button className="mr-3 bg-green-500 hover:bg-green-700 text-white font-bold  text-[0.5rem] sm:text-sm md:text-md lg:text-lg
                 px-1 py-2 lg:px-8 rounded">
                     Last 6 Months
                 </button></Link>
-                <Link href={{pathname:'tracks',query:{time_range:'long_term'}}}><button className="bg-green-500 hover:bg-green-700 text-white font-bold 
+                <Link href={{pathname:'tracks',query:{time_range:'long_term'}}}><button className="mr-3 bg-green-500 hover:bg-green-700 text-white font-bold  text-[0.5rem] sm:text-sm md:text-md lg:text-lg
                 px-1 py-2 lg:px-6 rounded">
                     All Time
                 </button></Link>
