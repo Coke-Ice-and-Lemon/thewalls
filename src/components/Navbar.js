9ba3f6177ac3203244e7ea2e057b2999d67beb05
Poojan-20 <poojanp2008@gmail.com>
2023-09-09 20:43:10 +0530

    Ui fixed

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -71,7 +71,7 @@ const Navbar = () => {
                                 <li className="nav-item">
                                     <Link
                                         className="px-3 py-2 h-full flex items-center text-xs uppercase font-bold leading-snug text-white hover:opacity-75"
-                                        href="/privacy-policy" onClick={() => {
+                                        onClick={() => {
                                             signOut()
                                             setNavbarOpen(false)
                                         }
