d58f6c82bf3a8bbe0e315f17b8266af90627e1cd
Poojan-20 <poojanp2008@gmail.com>
2023-09-16 01:56:16 +0530

    privacy policy changes

--- src/pages/privacy-policy.js
+++ src/pages/privacy-policy.js
@@ -10,24 +10,23 @@ const Privacypolicy = () => {
         <br />
         <br />
         <p>
-          Spotify was developed as an open source app powered by the
-          Spotify/Apple Music Web API. By choosing to use this app, you
+          The walls is an app powered by the Spotify web API. By choosing to use this app, you
           agree to the use of your Spotify account username and data for your top
           artists and tracks.
         </p>
         <br />
         <p>
-          None of the data used by Gramhophone is stored or collected anywhere, and
+          None of the data used by the walls is stored or collected anywhere, and
           it is NOT shared with any third parties. All information is used solely
           for displaying your Top Tracks .
         </p>
         <br />
         <p>
           Although you can rest assured that your data is not being stored or used
-          maliciously, if you would like to revoke Gramhophone&#39;s permissions, you
+          maliciously, if you would like to revoke the wall&#39;s permissions, you
           can visit
-          <a href="http://www.spotify.com/account/apps/?_ga=2.57194153.2059435232.1677244602-1044990631.1616788427">your apps page</a>
-          and click &#39;REMOVE ACCESS&#39; on Gramhophone.
+          <a href="http://www.spotify.com/account/apps/?_ga=2.57194153.2059435232.1677244602-1044990631.1616788427"> your apps page</a>
+          and click &#39;REMOVE ACCESS&#39; on the walls.
           <a href="https://support.spotify.com/us/article/spotify-on-other-apps/"> Here </a>
           is a more detailed guide for doing so.
         </p>
