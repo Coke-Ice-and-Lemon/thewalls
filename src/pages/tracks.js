1e83d0009b136fcd9d26ca1a15db3449d71a4470
Harsh Mathur <harshmathurx@gmail.com>
2023-11-16 16:07:16 +0530

    fetched items count increased

--- src/pages/tracks.js
+++ src/pages/tracks.js
@@ -142,7 +142,7 @@ const Tracks = ({ }) => {
     }
 
     async function getTopTracks(time) {
-        const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${time}&limit=50`, {
+        const response = await fetch(`https://api.spotify.com/v1/me/top/tracks?time_range=${time}&limit=100`, {
             headers: {
                 Authorization: `Bearer ${session.accessToken}`
             }
